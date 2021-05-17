f92807907b7f8ccf1bf09fc5b18017f70ca088cd
ShandonCodes <git@shandon.codes>
2021-05-16 20:23:32 -0400

    Added CSV output

--- scrape.go
+++ scrape.go
@@ -1,13 +1,37 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/gocolly/colly"
 )
 
+func writeCSV(rows [][]string) {
+	f, err := os.Create("./out.csv")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	defer f.Close()
+
+	w := csv.NewWriter(f)
+
+	for _, row := range rows {
+		w.Write(row)
+	}
+
+	f.Sync()
+
+	w.Flush()
+
+}
+
 func main() {
 	c := colly.NewCollector()
+	var rows [][]string
 
 	c.OnError(func(_ *colly.Response, err error) {
 		fmt.Println("Error:", err.Error())
@@ -19,8 +43,10 @@ func main() {
 
 	c.OnHTML("h2", func(e *colly.HTMLElement) {
 		fmt.Println(string(e.Text))
+		rows = append(rows, []string{string(e.Text)})
 	})
 
-	c.Visit("hps://en.wikipedia.org/wiki/Go_(programming_language)")
+	c.Visit("https://en.wikipedia.org/wiki/Go_(programming_language)")
+	writeCSV(rows)
 
 }
